f2a154f5fd2ebbb2d2f50ad5cfc5dc03e9b30d29
dorplaut <dorplaut@gmail.com>
2024-03-10 13:55:40 +0200

    change dirs names for vercel deployment

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './public/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public/dist'),
   },
   module: {
     rules: [
