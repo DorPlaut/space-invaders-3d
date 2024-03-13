9b76f23eb921ab883580f3b72c420ed86e0a7ef1
dorplaut <dorplaut@gmail.com>
2024-03-13 06:12:25 +0200

    test: serverless

--- api/db/connect.js
+++
@@ -1,9 +0,0 @@
-const moongose = require('mongoose');
-// const { options } = require('../routes/tasks');
-
-const connectDB = (url) => {
-  console.log('connectinng to DB..');
-  return moongose.connect(url, {});
-};
-
-module.exports = connectDB;
